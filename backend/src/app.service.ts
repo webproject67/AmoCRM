10b9dd171fa7628bfb917295914954b2494c437d
webproject67 <webproject67@gmail.com>
2023-05-30 09:15:22 +0300

    обменяет ключи на tokens

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@nestjs/common';
 
+interface IResult {
+  token_type: string;
+  expires_in: number;
+  access_token: string;
+  refresh_token: string;
+}
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getTokens(): Promise<any> {
+    const response = await fetch(
+      `https://${process.env.DOMAIN}.amocrm.ru/oauth2/access_token`,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          client_id: process.env.CLIENT_ID,
+          client_secret: process.env.CLIENT_SECRET,
+          grant_type: 'authorization_code',
+          code: process.env.CODE,
+          redirect_uri: 'http://localhost:3000',
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+
+    const result: IResult = await response.json();
   }
 }
