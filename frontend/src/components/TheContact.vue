cd7946c092a54cd1d16c8747a3c2630760401f10
webproject67 <webproject67@gmail.com>
2023-06-14 19:29:31 +0300

    исправит contact component

--- frontend/src/components/TheContact.vue
+++ frontend/src/components/TheContact.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-import { UserOutlined } from '@ant-design/icons-vue'
+import { UserOutlined, PhoneOutlined, MailOutlined } from '@ant-design/icons-vue'
 import TheAvatar from './TheAvatar.vue'
-import TheDivider from './TheDivider.vue'
 import { Communication } from '../const'
 
 const { PHONE, EMAIL } = Communication
@@ -27,7 +26,17 @@ defineProps<{
     {{ contact.name }}
     <template v-for="contacts in contact.custom_fields_values" :key="contacts">
       <template v-for="contact in contacts.values" :key="contact">
-        <TheDivider :contacts="contacts" :contact="contact" />
+        <a-divider type="vertical" />
+        <template v-if="contacts.field_code === PHONE">
+          <a :href="'tel:' + contact.value">
+            <phone-outlined />
+          </a>
+        </template>
+        <template v-if="contacts.field_code === EMAIL">
+          <a :href="'mailto:' + contact.value">
+            <mail-outlined />
+          </a>
+        </template>
       </template>
     </template>
   </div>
